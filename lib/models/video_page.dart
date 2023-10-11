f3fc1521c38a35dfb5fb8badd9f1ba8340b1e312
Farg0k <108218788+Farg0k@users.noreply.github.com>
2023-10-11 15:50:47 +0300

    fix description

--- lib/models/video_page.dart
+++ lib/models/video_page.dart
@@ -44,9 +44,9 @@ class VideoPage {
         channelName: map?['results']['results']['contents'][1]['videoSecondaryInfoRenderer']['owner']['videoOwnerRenderer']['title']['runs'][0]['text'],
         viewCount: map?['results']['results']['contents'][0]['videoPrimaryInfoRenderer']['viewCount']['videoViewCountRenderer']['shortViewCount']['simpleText'],
         subscribeCount: map?['results']?['results']?['contents']?[1]?['videoSecondaryInfoRenderer']?['owner']?['videoOwnerRenderer']?['subscriberCountText']?['simpleText'],
-        likeCount: map?['results']['results']['contents'][0]['videoPrimaryInfoRenderer']['videoActions']['menuRenderer']['topLevelButtons'][0]['toggleButtonRenderer']['defaultText']['simpleText'],
+        likeCount: map?['results']['results']['contents'][0]['videoPrimaryInfoRenderer']['videoActions']['menuRenderer']['topLevelButtons'][0]["segmentedLikeDislikeButtonRenderer"]["likeButton"]['toggleButtonRenderer']['defaultText']['simpleText'],
         unlikeCount: '',
-        description: map?['results']?['results']?['contents']?[1]?['videoSecondaryInfoRenderer']?['description']?['runs'],
+        description: map?['results']?['results']?['contents']?[1]?['videoSecondaryInfoRenderer']?["attributedDescription"]?["content"],
         date: map?['results']['results']['contents'][0]['videoPrimaryInfoRenderer']['dateText']['simpleText'],
         channelThumb: map?['results']['results']['contents'][1]['videoSecondaryInfoRenderer']['owner']['videoOwnerRenderer']['thumbnail']['thumbnails'][1]['url'],
         channelId: map?['results']['results']['contents'][1]['videoSecondaryInfoRenderer']['owner']['videoOwnerRenderer']['navigationEndpoint']['browseEndpoint']['browseId']
